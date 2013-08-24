c2e745e001bb42d3c98cc1fa3e600e7b2ee78eae
wT <wt@iki.fi>
2013-08-24 11:29:50 +0300

    Make restore_original.py and unpack.py Python2 compatible
    Repack.py is still messed up

--- restore_original.py
+++ restore_original.py
@@ -1,27 +1,36 @@
-#!python3
+#!/usr/bin/env python
 """
 Created on Aug 12, 2013
 
 @author: wT
-@version: 1.1
+@version: 1.2
 """
+
+from __future__ import print_function
 
 import sys
 sys.dont_write_bytecode = True # It's just clutter for this small scripts
-import os
+import shutil
 import traceback
 
-from unpack import check_for_correct_python, get_wad_path, get_wix_path
+from unpack import get_wad_path, get_wix_path
 
 
 if __name__ == '__main__':
 	try:
-		check_for_correct_python()
+		input = raw_input # Python 2 <-> 3 fix
+	except NameError:
+		pass
+	try:
 		yesno = input(r"Overwrite both (potentially modified) .wad and .wix files with originals? [y/n]: ").lower()
 		if yesno == "y" or yesno == "yes":
-			os.replace(get_wad_path() + ".orig", get_wad_path())
-			os.replace(get_wix_path() + ".orig", get_wix_path())
-			print("Done")
+			try:
+				shutil.move(get_wad_path() + ".orig", get_wad_path())
+				shutil.move(get_wix_path() + ".orig", get_wix_path())
+			except:
+				print("No .orig files to restore")
+			else:
+				print("Done")
 		else:
 			print("Not restoring")
 	except SystemExit as e:
--- unpack.py
+++ unpack.py
@@ -1,17 +1,20 @@
-#!python3
+#!/usr/bin/env python
 """
 Created on Aug 10, 2013
 
 @author: wT
-@version: 1.1
+@version: 1.2
 """
+
+from __future__ import print_function, unicode_literals
 
 import sys
 sys.dont_write_bytecode = True # It's just clutter for this small scripts
-import os
-import traceback
-import atexit
 
+import os
+import io
+import platform
+import traceback
 
 class PathStorage():
 	wad = None
@@ -36,55 +39,56 @@ def get_wix_path():
 	return PathStorage.wix
 
 def is_valid_input(input_file):
-		if input_file.endswith(".wad") and os.path.exists(input_file + ".wix"):
-			PathStorage.wad = input_file
-			PathStorage.wix = input_file + ".wix"
-		elif input_file.endswith(".wix"):
-			wad, _ = os.path.splitext(input_file)
-			PathStorage.wad = wad
-			PathStorage.wix = input_file
-		else:
-			return False
-		return True
+	if input_file.endswith(".wad") and os.path.exists(input_file + ".wix"):
+		PathStorage.wad = input_file
+		PathStorage.wix = input_file + ".wix"
+	elif input_file.endswith(".wix"):
+		wad, _ = os.path.splitext(input_file)
+		PathStorage.wad = wad
+		PathStorage.wix = input_file
+	else:
+		return False
+	return True
 
 def unpack_file(file_name, group, data):
 	dest_path = os.path.join("unpacked", group)
 	if not os.path.exists(dest_path):
-		os.makedirs(dest_path, exist_ok=True)
+		os.makedirs(dest_path)
 
-	with open(os.path.join(dest_path, file_name), mode="wb") as output:
+	with io.open(os.path.join(dest_path, file_name), mode="wb") as output:
 		output.write(data)
 		print("Unpacked", file_name)
 
-def backup_files():
+def prepare_directory():
+	# First make backups
+	import shutil
 	index_file = get_wix_path()
 	wad_file = get_wad_path()
-	import shutil
 	if not os.path.exists(index_file + ".orig"):
 		shutil.copy2(index_file, index_file + ".orig")
 	if not os.path.exists(wad_file + ".orig"):
 		shutil.copy2(wad_file, wad_file + ".orig")
-
-def check_for_correct_python():
-	major_version = sys.version_info[0]
-	if major_version < 3:
-		sys.exit("Please use Python 3.x")
+	# Secondly create repack dir if it's not there yet
+	if not os.path.exists('repack'):
+		os.mkdir("repack")
 
 def error_out_bad_input():
 	sys.exit("I need a .wad or .wix file, ya dingus")
 
-@atexit.register
-def pause_on_exit():
-	# input("Press enter to close")
-	os.system("pause") # os.system'ing anything is supposedly awful. Let's keep it for now
+if platform.system() == "Windows":
+	import atexit
+	@atexit.register
+	def pause_on_exit():
+		# input("Press enter to close")
+		os.system("pause") # os.system'ing anything is supposedly awful. Let's keep it for now
+	del atexit
 
 if __name__ == '__main__':
 	try:
-		check_for_correct_python()
-		backup_files()
+		prepare_directory()
 
-		with open(get_wad_path(), mode='rb') as wad_file:
-			with open(get_wix_path(), mode="r") as index_file:
+		with io.open(get_wad_path(), mode='rb') as wad_file:
+			with io.open(get_wix_path(), mode="r") as index_file:
 				for line in index_file:
 					if line.startswith("!group"):
 						_, group = line.split()
@@ -92,8 +96,10 @@ if __name__ == '__main__':
 						file_name, offset, size = line.split()
 						wad_file.seek(int(offset))
 						data = wad_file.read(int(size))
+						if len(data) == 0:
+							print("Error while unpacking. Hit end-of-file before we should've. Is {} corrupted?".format(get_wad_path()))
+							break
 						unpack_file(file_name, group, data)
-		os.makedirs("repack", exist_ok=True)
 	except SystemExit as e:
 		print(e)
 	except:
